7037caad5058866ad5d7ffe8c834afc144a5553e
dimpz2518 <79840997+dimpz2518@users.noreply.github.com>
2021-03-01 20:05:33 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws"{
+  access_key="AKIAIGI2FJWTDRHZBVOA"
+  secret_key="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"
+  region="ap-south-1"
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "ami"{
+default = "ami-0eeb03e72075b9bcc"
+}
+variable "instance_type"{
+default = "t3.micro"
+}
